398a5def58601917221663698a78b0a940ec252e
daris <darisarsen24@gmail.com>
2024-04-07 09:07:58 +0500

    commit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,8 +4,6 @@ import { WeatherService } from './weather.service';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
-import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 
 
 @Component({
