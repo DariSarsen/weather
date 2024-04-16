b023e2faa04887b22b3235d891f8ffc5502cec59
daris <darisarsen24@gmail.com>
2024-04-16 10:19:46 +0500

    .

--- frontend/cypress/e2e/spec-copy-1.cy.ts
+++ frontend/cypress/e2e/spec-copy-1.cy.ts
@@ -4,7 +4,7 @@ describe('Weather App', () => {
   })
 
   it('can enter a city name', () => {
-    cy.get('[data-testid="input-city"]').first().type('Almaty')
+    cy.get('[data-testid="input-city"]', { timeout: 10000 }).first().type('Almaty')
     cy.get('[data-testid="get-weather-btn"]').first().click()
     cy.get('.weather-details').should('contain', 'Almaty')
   })
